30265979cbeb4ab3a04d3b886aae23c1e0ae5c81
Christopher Harrison <ch12@sanger.ac.uk>
2020-01-16 13:07:17 +0000

    This method is a bit nicer...but still a bit misguided :P

--- db.py
+++ db.py
@@ -18,14 +18,16 @@ with this program. If not, see https://www.gnu.org/licenses/
 """
 
 import os
-from contextlib import contextmanager
+from abc import ABCMeta, abstractmethod
+from contextlib import AbstractContextManager
 from enum import Enum
 from functools import singledispatch
 from pathlib import Path
 
-import psycopg2
+from psycopg2 import Error as PGError
 from psycopg2.errors import RaiseException
-from psycopg2.extras import NamedTupleCursor
+from psycopg2.extensions import cursor as Cursor, connection as Connection
+from psycopg2.extras import NamedTupleCursor, execute_batch
 from psycopg2.pool import AbstractConnectionPool, ThreadedConnectionPool
 from psycopg2.sql import SQL, Identifier
 
@@ -40,63 +42,9 @@ _POOL_MIN = int(os.getenv("PG_POOL_MIN", "1"))
 _POOL_MAX = int(os.getenv("PG_POOL_MAX", "10"))
 
 
-# Advisory lock IDs
-_ADVISORY_LOCKS = {
-    "DDL": 0
-}
-
-
-_ExcT = T.TypeVar("_ExcT", bound=BackendException)
-
-def _exception(heading:str, pg_exc:psycopg2.Error, exc_type:T.Type[_ExcT]) -> _ExcT:
-    message = f"{heading} {pg_exc.pgcode}"
-    if pg_exc.pgerror:
-        message += f"\n{pg_exc.pgerror}"
-
-    return exc_type(message)
-
-@singledispatch
-def _exception_mapper(exc:psycopg2.Error) -> BackendException:
-    # Fallback to BackendException
-    return _exception("PostgreSQL error", exc, BackendException)
-
-@_exception_mapper.register
-def _(exc:RaiseException) -> LogicException:
-    # RaiseException -> LogicException
-    return _exception("PL/pgSQL exception", exc, LogicException)
-
-
-def _session_factory(init_state, extract_cursor, teardown, commit_session, rollback_session):
-    """
-    Session context manager factory
-
-    @param   init_state        Callable that initialises state
-    @param   extract_cursor    Callable that returns a cursor from state
-    @param   teardown          Callable that tears down state
-    @param   commit_session    Callable that commits the session
-    @param   rollback_session  Callable that rolls the session back
-    @return  Session context manager
-    """
-    def _session():
-        failed = False
-
-        try:
-            state = init_state()
-            yield extract_cursor(state)
-
-        except psycopg2.Error as e:
-            failed = True
-            rollback_session(state)
-            raise _exception_mapper(e)
-
-        finally:
-            if not failed:
-                commit_session(state)
-
-            teardown(state)
-
-    return contextmanager(_session)
-
+class AdvisoryLockID(Enum):
+    """ Enumeration of application-specific advisory lock IDs """
+    DDL = 0
 
 class LockingMode(Enum):
     """ PostgreSQL locking mode enumeration """
@@ -110,44 +58,115 @@ class LockingMode(Enum):
     AccessExclusive      = "access exclusive"
 
 
+_ExcT = T.TypeVar("_ExcT", bound=BackendException)
+
+def _exception(heading:str, pg_exc:PGError, exc_type:T.Type[_ExcT]) -> _ExcT:
+    message = f"{heading} {pg_exc.pgcode}"
+    if pg_exc.pgerror:
+        message += f"\n{pg_exc.pgerror}"
+
+    return exc_type(message)
+
+@singledispatch
+def _exception_mapper(exc:PGError) -> BackendException:
+    # Fallback to BackendException
+    return _exception("PostgreSQL error", exc, BackendException)
+
+@_exception_mapper.register
+def _(exc:RaiseException) -> LogicException:
+    # RaiseException -> LogicException
+    return _exception("PL/pgSQL exception", exc, LogicException)
+
+class _BaseSession(AbstractContextManager, metaclass=ABCMeta):
+    """ Abstract base class for session context managers """
+    _connection:Connection
+    _cursor:Cursor
+
+    def __enter__(self) -> Cursor:
+        self.session()
+        return self._cursor
+
+    def __exit__(self, *exception) -> bool:
+        _, exc, _ = exception
+        raised = exc is not None
+
+        try:
+            if raised:
+                self._connection.rollback()
+                if isinstance(exc, PGError):
+                    raise _exception_mapper(exc)
+            else:
+                self._connection.commit()
+
+        finally:
+            self.teardown()
+
+        return False
+
+    @abstractmethod
+    def session(self) -> None:
+        """ Initialise session state """
+
+    @abstractmethod
+    def teardown(self) -> None:
+        """ Teardown session state """
+
+
 class _LockableNamedTupleCursor(NamedTupleCursor):
     """ NamedTupleCursor with a locking context managers """
-    def advisory_lock(self, lock_id:int):
-        """
-        Acquire a session-level advisory lock context manager with the
-        given advisory lock ID
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
-        @oaram  lock_id  Advisory lock ID
-        """
-        def _init_state():
-            self.execute("select pg_advisory_lock(%s);", (lock_id,))
-            return self
+        cursor = self
 
-        def _teardown(_):
-            self.execute("select pg_advisory_unlock(%s);", (lock_id,))
+        class _Lock(_BaseSession):
+            """ Base lock """
+            _entry_query:T.Optional[T.Tuple[T.Union[str, SQL], ...]]
+            _exit_query:T.Optional[T.Tuple[T.Union[str, SQL], ...]]
 
-        return _session_factory(_init_state, (lambda x: x), _teardown, (lambda _: self.connection.commit()), (lambda _: self.connection.rollback()))()
+            def session(self) -> None:
+                self._connection = cursor.connection
+                self._cursor = cursor
 
-    def table_lock(self, *tables:str, lock_mode:LockingMode = LockingMode.AccessExclusive):
-        """
-        Acquire a table lock context manager on the given tables with
-        the specified locking mode
+                if self._entry_query is not None:
+                    self._cursor.execute(*self._entry_query)
 
-        @param  tables     PostgreSQL tables
-        @param  lock_mode  Locking mode (defaults to "access exclusive")
-        """
-        # NOTE Given that we've effectively serialised everything with
-        # advisory locks, explicit table locking is a bit redundant
-        def _init_state():
-            to_lock = SQL(", ").join(Identifier(t) for t in tables)
-            self.execute(SQL("""
-                begin transaction;
-                lock table {tables} in """ f"{lock_mode.value}" """ mode;
-            """).format(tables=to_lock))
+            def teardown(self) -> None:
+                if self._exit_query is not None:
+                    self._cursor.execute(*self._exit_query)
 
-            return self
+        class _AdvisoryLock(_Lock):
+            """
+            Acquire a session-level advisory lock context manager with
+            the given advisory lock ID
 
-        return _session_factory(_init_state, (lambda x: x), (lambda _: None), (lambda _: self.connection.commit()), (lambda _: self.connection.rollback()))()
+            @oaram  lock_id  Advisory lock ID
+            """
+            def __init__(self, lock_id:AdvisoryLockID):
+                self._entry_query = ("select pg_advisory_lock(%s);", (lock_id.value,))
+                self._exit_query  = ("select pg_advisory_unlock(%s);", (lock_id.value,))
+
+        class _TableLock(_Lock):
+            """
+            Acquire a table lock context manager on the given tables
+            with the specified locking mode
+
+            @param  tables     PostgreSQL tables
+            @param  lock_mode  Locking mode (defaults to "access exclusive")
+            """
+            # NOTE Given that we've effectively serialised everything
+            # with advisory locks, explicit table locking is a bit
+            # redundant
+            def __init__(self, *tables:str, lock_mode:LockingMode = LockingMode.AccessExclusive):
+                to_lock = SQL(", ").join(Identifier(t) for t in tables)
+                self._entry_query = (SQL("""
+                    begin transaction;
+                    lock table {tables} in """ f"{lock_mode.value}" """ mode;
+                """).format(tables=to_lock),)
+                self._exit_query = None
+
+        self.advisory_lock = _AdvisoryLock
+        self.table_lock = _TableLock
 
 
 class PostgreSQL(BaseStateProtocol):
@@ -163,44 +182,43 @@ class PostgreSQL(BaseStateProtocol):
         self._filesystems = {}
 
         dsn = f"dbname={database} user={user} password={password} host={host} port={port}"
-        self._pool = ThreadedConnectionPool(_POOL_MIN, _POOL_MAX, dsn, cursor_factory=_LockableNamedTupleCursor)
+        self._pool = pool = ThreadedConnectionPool(_POOL_MIN, _POOL_MAX, dsn, cursor_factory=_LockableNamedTupleCursor)
+
+        class _Transaction(_BaseSession):
+            """
+            Transaction context manager, using the connection pool
+
+            @param  autocommit  Transaction autocommit mode (defaults to False)
+            """
+            _pool:AbstractConnectionPool
+            _autocommit:bool
+
+            def __init__(self, *, autocommit:bool = False) -> None:
+                self._pool = pool
+                self._autocommit = autocommit
+
+            def session(self) -> None:
+                self._connection = self._pool.getconn()
+                self._connection.autocommit = self._autocommit
+                self._cursor = self._connection.cursor()
+
+                # Acquire and release all advisory locks
+                # NOTE This will necessarily serialise transactions
+                execute_batch(self._cursor, """
+                    select pg_advisory_lock(%(lock_id)s);
+                    select pg_advisory_unlock(%(lock_id)s);
+                """, [{"lock_id": lock_id} for lock_id in AdvisoryLockID])
+
+                # Start transaction
+                self._cursor.execute("begin transaction;")
+
+            def teardown(self) -> None:
+                self._pool.putconn(self._connection)
+
+        self.transaction = _Transaction
 
     def __del__(self) -> None:
         self._pool.closeall()
-
-    def transaction(self, *, autocommit:bool = False):
-        """ Transaction context manager, using the connection pool """
-        def _init_state():
-            connection = self._pool.getconn()
-            connection.autocommit = autocommit
-            cursor = connection.cursor()
-
-            # Acquire and release all advisory locks
-            # NOTE This will necessarily serialise transactions
-            cursor.executemany("""
-                select pg_advisory_lock(%(lock_id)s);
-                select pg_advisory_unlock(%(lock_id)s);
-            """, [{"lock_id": lock_id} for lock_id in _ADVISORY_LOCKS.values()])
-
-            return connection, cursor
-
-        def _extract_cursor(state):
-            _, cursor = state
-            return cursor
-
-        def _teardown(state):
-            connection, _ = state
-            self._pool.putconn(connection)
-
-        def _commit_session(state):
-            connection, _ = state
-            connection.commit()
-
-        def _rollback_session(state):
-            connection, _ = state
-            connection.rollback()
-
-        return _session_factory(_init_state, _extract_cursor, _teardown, _commit_session, _rollback_session)()
 
     def execute_script(self, sql:Path) -> None:
         """
@@ -209,7 +227,7 @@ class PostgreSQL(BaseStateProtocol):
         @param  sql  Path to SQL script
         """
         with self.transaction(autocommit=True) as c:
-            with c.advisory_lock(_ADVISORY_LOCKS["DDL"]):
+            with c.advisory_lock(AdvisoryLockID.DDL):
                 c.execute(sql.read_text())
 
     def filesystem_convertor(self, name:str) -> BaseFilesystem:
