12e838008a95e0c6624e506b9c7ff730cf19246f
Christopher Harrison <ch12@sanger.ac.uk>
2020-05-05 15:41:07 +0100

    Added stub test runner, with loose spec (from design)
    Added:
    * Stub of test runner script for CI

---
+++ .ci/run-tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# TODO Test Runner
+#
+# * Run tests (exit non-zero on failure)
+#
+# * Establish hot, warm and cool code (hot is anything in hot/, warm is
+#   anything referenced by hot, cool is everything else)
+#
+#   * Hot code must have: Coverage = 100%; McCabe <= 5
+#   * Warm code must have: Coverage > 90%; McCabe <= 5
+#   * Cool code must have: Coverage > 80%; McCabe <= 10 (warn if > 7)
+#
+#   Anything not satisfying the above should exit non-zero
+#
+# * PEP8 and type checking (maybe warn, rather than fail, because of
+#   false positives)
