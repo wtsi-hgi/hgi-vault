a66c65135c10c6dc2615df1bbc59f9957a902299
Christopher Harrison <ch12@sanger.ac.uk>
2020-05-19 17:39:36 +0100

    Allow a vault key to be recovered from a path
    Added:
    * _VaultFileKey.alternate method to locate alternate keys, if they
      exist. NOTE: This has a bug in it, because it needs to be dependant on
      the branch. It needs to move to VaultFile, with a suitable interface
      exposed in _VaultFileKey to avoid relying on internal details
    
    Changed:
    * Construction of _VaultFileKey on either inode+path or recovered from
      key path
    * _VaultFileKey inode must be passed explicitly, as we're not storing
      enough information to resolve it
    * Tests for _VaultFileKey

--- models/vault.py
+++ models/vault.py
@@ -19,6 +19,8 @@ with this program. If not, see https://www.gnu.org/licenses/
 
 from __future__ import annotations
 
+import os.path
+import re
 from dataclasses import dataclass
 from os import PathLike
 
@@ -28,51 +30,134 @@ from core.utils import base64
 from core.vault import Branch, base, exception
 
 
-_KEY_DELIMITER = "-"
+_PrefixSuffixT = T.Tuple[T.Optional[T.Path], T.Path]
 
 @dataclass(init=False)
 class _VaultFileKey(PathLike):
     """ HGI vault file key properties """
-    # FIXME? I'm not sure it's necessary to separate this out into a
-    # separate class; it could easily be a part of VaultFile. It does
-    # make testing a bit easier, though...
-    prefix:T.Path  # inode prefix path, without the LSB
-    suffix:T.Path  # LSB and basename suffix path
-    glob:str       # Suffix glob
+    # NOTE This is implemented in a separate class to keep that part of
+    # the logic outside of VaultFile. This isn't strictly necessary and
+    # MAY get merged into VaultFile as the implementation develops...
+    _delimiter:T.ClassVar[str] = "-"
 
-    def __init__(self, path:T.Path) -> None:
+    prefix:T.Optional[T.Path]  # inode prefix path, without the LSB
+    suffix:T.Path              # LSB and encoded basename suffix path
+
+    def __init__(self, *, inode:T.Optional[int]       = None,
+                          path:T.Optional[T.Path]     = None,
+                          key_path:T.Optional[T.Path] = None) -> None:
+        """
+        Construct the key either from an inode-path tuple, or recovered
+        from a key's path
+
+        @param   inode     inode ID to construct from
+        @param   path      Path to construct from
+        @param   key_path  Key path to recover from
+        """
+        try:
+            # NOTE inode-path and key_path are mutually exclusive and
+            # one MUST be provided. It would be messy to do this with
+            # single dispatch, so we roll our own multiple dispatch...
+            # which is equally messy :P
+            dispatch, *args = {
+                (False, False, True):  (self._construct, inode, path),
+                (True,  True,  False): (self._recover, key_path)
+            }[inode is None, path is None, key_path is None]
+        except KeyError:
+            raise TypeError(f"{self.__class__.__name__} must be constructed with inode and path arguments OR from a key path")
+
+        self.prefix, self.suffix = dispatch(*args)
+
+    def _construct(self, inode:int, path:T.Path) -> _PrefixSuffixT:
+        """ Construct the vault file key from an inode ID and path """
         # The byte-padded hexidecimal representation of the inode ID
-        inode = f"{file.inode_id(path):x}"
-        if len(inode) % 2:
-            inode = f"0{inode}"
+        inode_hex = f"{inode:x}"
+        if len(inode_hex) % 2:
+            inode_hex = f"0{inode_hex}"
 
         # Chunk the inode ID into 8-bit segments
-        chunks = [inode[i:i+2] for i in range(0, len(inode), 2)]
+        chunks = [inode_hex[i:i+2] for i in range(0, len(inode_hex), 2)]
 
-        # inode ID, without the least significant byte
-        self.prefix = T.Path(*chunks[:-1])
+        # inode ID, without the least significant byte, if it exists
+        dirname = None
+        if len(chunks) > 1:
+            dirname = T.Path(*chunks[:-1])
 
         # inode ID LSB, delimiter, and the base64 encoding of the path
-        basename_prefix = chunks[-1] + _KEY_DELIMITER
-        self.suffix = T.Path(basename_prefix + base64.encode(str(path)))
-        self.glob = f"{basename_prefix}*"
+        prefix = chunks[-1] + self._delimiter
+        basename = T.Path(prefix + base64.encode(str(path)))
+
+        return dirname, basename
+
+    def _recover(self, path:T.Path) -> _PrefixSuffixT:
+        """ Recover the vault file key from a vault file key path """
+        dirname, basename = os.path.split(path)
+        prefix = T.Path(dirname) if dirname else None
+        return prefix, T.Path(basename)
+
+    def __eq__(self, rhs:_VaultFileKey) -> bool:
+        return self.prefix == rhs.prefix \
+           and self.suffix == rhs.suffix
+
+    def __bool__(self) -> bool:
+        return True
 
     def __fspath__(self) -> str:
         return str(self.path)
 
     @property
     def path(self) -> T.Path:
-        return T.Path(self.prefix, self.suffix)
+        return self.suffix if self.prefix is None else T.Path(self.prefix, self.suffix)
+
+    @property
+    def source(self) -> T.Path:
+        """ Return the source file path """
+        _, encoded = self.suffix.name.split(self._delimiter)
+        return T.Path(base64.decode(encoded).decode())
+
+    def alternate(self, search_base:T.Path) -> T.Optional[_VaultFileKey]:
+        """
+        Return an alternate key if one exists in the given search base
+
+        @param   search_base  Search base path
+        @return  Alternate key (None, if not found)
+        """
+        # NOTE This method couples this class to the filesystem, however
+        # it relies on its internal implementation details, so it makes
+        # sense for it to belong here. However, this coupling gives
+        # justification for merging this class into VaultFile...
+        if self.prefix is not None:
+            search_base = search_base / self.prefix
+
+        lsb, _ = self.suffix.name.split(self._delimiter)
+
+        try:
+            # NOTE This assumes there's only ever one match
+            alt, *_ = search_base.glob(f"{lsb}{self._delimiter}*")
+            alt = T.Path(alt.name)
+        except ValueError:
+            # Alternate not found
+            return None
+
+        if alt == self.suffix:
+            # Alternate not found
+            # FIXME This is wrong; it's dependant upon the branch!
+            return None
+
+        if self.prefix is not None:
+            alt = self.prefix / alt
+
+        return _VaultFileKey(key_path=alt)
 
 
 class VaultFile(base.VaultFile):
     """ HGI vault file implementation """
+    # TODO Do we need _path, now we've got _VaultFileKey.source?
     _path:T.Path        # Path to external (non-vaulted) file (relative to vault root)
     _key:_VaultFileKey  # Vault key (path) of external file
 
     def __init__(self, vault:Vault, branch:Branch, path:T.Path) -> None:
-        self.vault  = vault
-        self.branch = branch
+        self.vault = vault
 
         if not path.exists():
             raise exception.DoesNotExist(f"{path} does not exist")
@@ -80,12 +165,27 @@ class VaultFile(base.VaultFile):
         if not file.is_regular(path):
             raise exception.NotRegularFile(f"{path} is not a regular file")
 
-        self._path = self._relative_path(path)
-        self._key = _VaultFileKey(self._path)
+        self.branch = branch
+        self._path  = self._relative_path(path)
+        self._key   = _VaultFileKey(self._path)
 
-        # TODO Check the key to see if it already exists under a
-        # different name; indicating that the given path has been
-        # vaulted, but renamed/moved.
+        # Test for alternatives in the given branch
+        this_branch = self._key.alternate(vault.location / branch.value)
+        if this_branch is not None:
+            # TODO This should be logged: Source file and vaulted file's
+            # names differ (i.e., source file renamed)
+            self._path  = this_branch.source
+            self._key   = this_branch
+
+        else:
+            # Test for alternatives in the other branch
+            other_branch = self._key.alternate(vault.location / (~branch).value)
+            if other_branch is not None:
+                # TODO This should be logged: Branch and/or source name changed
+                self.branch = ~branch
+                self._path  = other_branch.source
+                self._key   = other_branch
+
         # TODO Check number of hardlinks on key, if it exists; if it is
         # one, then the original file has been removed. Do what??
 
--- test/models/test_vault.py
+++ test/models/test_vault.py
@@ -18,35 +18,66 @@ with this program. If not, see https://www.gnu.org/licenses/
 """
 
 import unittest
-from unittest.mock import patch
+from tempfile import TemporaryDirectory
 
 from core import typing as T
 from core.utils import base64
 from models.vault import _VaultFileKey
 
 
+_DUMMY = "foo/bar/quux"
+_DUMMY_FILE = T.Path(_DUMMY)
+_DUMMY_ENC = base64.encode(_DUMMY)
+
+# For convenience
+VFK = lambda inode, path: _VaultFileKey(inode=inode, path=path)
+VFK_k = lambda path: _VaultFileKey(key_path=path)
+
 class TestVaultFileKey(unittest.TestCase):
-    @patch("core.file.inode_id")
-    def test_constructor(self, mock_inode_id):
-        dummy      = "foo/bar/quux"
-        dummy_file = T.Path(dummy)
-        b64_dummy  = base64.encode(dummy)
+    def test_constructor(self):
+        self.assertEqual(VFK(0x1,    _DUMMY_FILE).path, T.Path(f"01-{_DUMMY_ENC}"))
+        self.assertEqual(VFK(0x1,    _DUMMY_FILE).path, T.Path(f"01-{_DUMMY_ENC}"))
+        self.assertEqual(VFK(0x12,   _DUMMY_FILE).path, T.Path(f"12-{_DUMMY_ENC}"))
+        self.assertEqual(VFK(0x123,  _DUMMY_FILE).path, T.Path(f"01/23-{_DUMMY_ENC}"))
+        self.assertEqual(VFK(0x1234, _DUMMY_FILE).path, T.Path(f"12/34-{_DUMMY_ENC}"))
 
-        mock_inode_id.return_value = 0x1
-        self.assertEqual(_VaultFileKey(dummy_file).path,
-                         T.Path(f"01-{b64_dummy}"))
+        self.assertRaises(TypeError, _VaultFileKey)
+        self.assertRaises(TypeError, _VaultFileKey, inode=123)
+        self.assertRaises(TypeError, _VaultFileKey, path=_DUMMY_FILE)
+        self.assertRaises(TypeError, _VaultFileKey, inode=123, key_path=_DUMMY_FILE)
+        self.assertRaises(TypeError, _VaultFileKey, path=_DUMMY_FILE, key_path=_DUMMY_FILE)
+        self.assertRaises(TypeError, _VaultFileKey, inode=123, path=_DUMMY_FILE, key_path=_DUMMY_FILE)
 
-        mock_inode_id.return_value = 0x12
-        self.assertEqual(_VaultFileKey(dummy_file).path,
-                         T.Path(f"12-{b64_dummy}"))
+    def test_resolve(self):
+        self.assertEqual(VFK(0, _DUMMY_FILE).source, _DUMMY_FILE)
+        self.assertEqual(VFK_k(T.Path(f"01-{_DUMMY_ENC}")).source, _DUMMY_FILE)
 
-        mock_inode_id.return_value = 0x123
-        self.assertEqual(_VaultFileKey(dummy_file).path,
-                         T.Path(f"01/23-{b64_dummy}"))
+    def test_equality(self):
+        self.assertEqual(VFK(0x12,  _DUMMY_FILE), VFK_k(T.Path(f"12-{_DUMMY_ENC}")))
+        self.assertEqual(VFK(0x123, _DUMMY_FILE), VFK_k(T.Path(f"01/23-{_DUMMY_ENC}")))
 
-        mock_inode_id.return_value = 0x1234
-        self.assertEqual(_VaultFileKey(dummy_file).path,
-                         T.Path(f"12/34-{b64_dummy}"))
+    def test_alternate(self):
+        key = VFK(0x1234, _DUMMY_FILE)
+
+        alternate = "some/other/path"
+        alt_key = VFK(0x1234, T.Path(alternate))
+
+        with TemporaryDirectory() as tmp:
+            tmp = T.Path(tmp)
+            (tmp / "12").mkdir()
+
+            # No alternate exists
+            self.assertIsNone(key.alternate(tmp))
+
+            # Original key exists
+            original = f"12/34-{_DUMMY_ENC}"
+            (tmp / original).touch()
+            self.assertIsNone(key.alternate(tmp))
+
+            # Create alternate
+            alt_enc = f"12/34-{base64.encode(alternate)}"
+            (tmp / original).rename(tmp / alt_enc)
+            self.assertEqual(key.alternate(tmp), alt_key)
 
 
 if __name__ == "__main__":
