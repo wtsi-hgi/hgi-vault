39c8cb10ce5baa57985c9ce581d88a53512857f2
Piyush Ahuja <piyushahuja.in@gmail.com>
2020-05-26 18:40:08 +0100

    Code Coverage packages and tests
    Added:
    * Basic infra (configuration file, packages) for testing code coverage thresholds
    * Test runner exits non-zero code when coverage thresholds are not met

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -16,10 +16,40 @@
 # * PEP8 and type checking (maybe warn, rather than fail, because of
 #   false positives)
 
+set -e
 # Ensure we're in the correct directory
 declare BASE="$(git rev-parse --show-toplevel)"
 [[ "$(pwd)" != "${BASE}" ]] && cd "${BASE}"
 
-nose2 --fail-fast \
-      --with-coverage --coverage-report=term-missing \
-      --verbose
+die() {
+  echo -e "$2" >&2
+  exit $1
+}
+
+
+# nose2 --fail-fast \
+#       --with-coverage --coverage-report=term-missing \
+#       --verbose
+
+
+
+# Why coverage and not nose2-cov? We are using the coverage module to invoke test runner (nose2) rather than using a plugin for test runner to report coverage (nose2-cov), because nose-2 cov doesnt seem to have a --fail-under option.   https://stackoverflow.com/questions/52205363/have-nosetests-ignore-minimum-coverage-if-exit-due-to-test-failure
+
+
+coverage run -m nose2 --fail-fast  --coverage-report=term-missing \
+   --verbose
+
+coverage report --rcfile=.hotCoveragerc  || die 1 "Hot Coverage is not satisfied"
+coverage report --rcfile=.warmCoveragerc || die 1 "Warm Coverage is not satisfied"
+coverage report --rcfile=.coldCoveragerc || die 1 "Cold Coverage is not satisfied"
+
+
+
+
+
+
+# radon cc -s --total-average -nb -j --json hot/
+
+# must conform to the PEP8 style: Pylint is a Python static code analysis tool which looks for programming errors, helps enforcing a coding standard, sniffs for code smells and offers simple refactoring suggestions.
+
+# Mypy: The system's code must be fully type annotated and satisfy static analysis.
