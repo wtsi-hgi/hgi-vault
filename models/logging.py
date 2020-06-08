047b705a72c9ae12bd2a609e859fc68c549075cb
Christopher Harrison <ch12@sanger.ac.uk>
2020-06-08 12:39:13 +0100

    Changed core logging module to work as a mixin
    Added:
    * models.logging provides a common mixin for all non-specific classes
    
    Changed:
    * core.logging such that is exposes a mixin base class, rather than a
      simple function. It provides the same interface so few downstream
      changes are necessary, but it allows easier definition of custom
      loggers on different classes. (Note that logging is class-based now;
      however one could instantiate models.logging.Loggable for
      module/function-based logging)

---
+++ models/logging.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) 2019, 2020 Genome Research Limited
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see https://www.gnu.org/licenses/
+"""
+
+from core.logging import base, utils, levels, formats
+
+
+class Loggable(base.LoggableMixin):
+    # Default/base loggable mixin
+    _logger    = "vault"
+    _level     = levels.default
+    _formatter = formats.default
+
+utils.to_tty(Loggable)
+utils.set_exception_handler(Loggable)
