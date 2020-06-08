10ceb9eb7b8dc946a22455fd02803a24e840a8bd
Christopher Harrison <ch12@sanger.ac.uk>
2020-06-08 12:39:13 +0100

    Changed core logging module to work as a mixin
    Added:
    * models.logging provides a common mixin for all non-specific classes
    
    Changed:
    * core.logging such that is exposes a mixin base class, rather than a
      simple function. It provides the same interface so few downstream
      changes are necessary, but it allows easier definition of custom
      loggers on different classes. (Note that logging is class-based now;
      however one could instantiate models.logging.Loggable for
      module/function-based logging)

--- core/logging.py
+++ core/logging.py
@@ -20,30 +20,12 @@ with this program. If not, see https://www.gnu.org/licenses/
 import logging
 import sys
 from enum import Enum
+from functools import partial
+from getpass import getuser
 from traceback import print_tb
 from types import TracebackType
 
 from . import typing as T, time
-
-
-def _exception_handler(logger:logging.Logger) -> T.Callable:
-    """
-    Create an exception handler that logs uncaught exceptions (except
-    keyboard interrupts) and spews the traceback to stderr (in debugging
-    mode) before terminating
-    """
-    def _log_uncaught_exception(exc_type:T.Type[Exception], exc_val:Exception, traceback:TracebackType) -> None:
-        if issubclass(exc_type, KeyboardInterrupt):
-            sys.__excepthook__(exc_type, exc_val, traceback)
-
-        else:
-            logger.critical(str(exc_val) or exc_type.__name__)
-            if __debug__:
-                print_tb(traceback)
-
-            sys.exit(1)
-
-    return _log_uncaught_exception
 
 
 class Level(Enum):
@@ -55,63 +37,124 @@ class Level(Enum):
     Critical = logging.CRITICAL
 
 
-class Logger:
-    _logger:logging.Logger
+class _LoggableMixin:
+    """ Base mixin class for logging interface """
+    # NOTE The following can be either class or instance variables
+    # NOTE _logger refers to a named logger with a set level; specifying
+    # a different _level will change that value for that logger, but not
+    # any handlers that have already been defined. This is a bit messy,
+    # but it's to facilitate the easy definition of downstream mixins
+    _logger:str
     _level:Level
-    _format:logging.Formatter
+    _formatter:logging.Formatter
 
-    def __init__(self, name:str, level:Level, formatter:logging.Formatter) -> None:
-        self._logger = logging.getLogger(name)
-        self._level  = level
-        self._format = formatter
+    @property
+    def logger(self) -> logging.Logger:
+        logger = logging.getLogger(self._logger)
+        logger.setLevel(self._level.value)
+        return logger
 
-        # NOTE It's a client's responsibility to add a useful output
-        # handler (see .to_tty and .to_file, for convenience)
-        self._logger.setLevel(level.value)
-        self._add_handler(logging.NullHandler())
-
-        sys.excepthook = _exception_handler(self._logger)
-
-    def _add_handler(self, handler:logging.Handler) -> None:
-        handler.setLevel(self._level.value)
-        handler.setFormatter(self._format)
-        self._logger.addHandler(handler)
-
-    def to_tty(self) -> None:
-        """ Add a handler to the standard output streams """
-        self._add_handler(logging.StreamHandler())
-
-    def to_file(self, filename:T.Path) -> None:
-        """ Add a handler to a given file """
-        self._add_handler(logging.FileHandler(filename))
-
-    def __call__(self, message:str, level:Level = Level.Info) -> None:
+    def _log(self, message:str, level:Level = Level.Info) -> None:
         """ Log a message at an optional level """
-        self._logger.log(level.value, message)
+        self.logger.log(level.value, message)
 
-    def debug(self, message:str) -> None:
-        # Convenience alias
-        self(message, Level.Debug)
+    @property
+    def log(self) -> object:
+        """ End-user logging functions exposed as log.* """
+        parent = self
 
-    def info(self, message:str) -> None:
-        # Convenience alias
-        self(message, Level.Info)
+        class _wrapper:
+            @staticmethod
+            def debug(message:str) -> None:
+                # Convenience alias
+                parent._log(message, Level.Debug)
 
-    def warning(self, message:str) -> None:
-        # Convenience alias
-        self(message, Level.Warning)
+            @staticmethod
+            def info(message:str) -> None:
+                # Convenience alias
+                parent._log(message, Level.Info)
 
-    def error(self, message:str) -> None:
-        # Convenience alias
-        self(message, Level.Error)
+            @staticmethod
+            def warning(message:str) -> None:
+                # Convenience alias
+                parent._log(message, Level.Warning)
 
-    def critical(self, message:str) -> None:
-        # Convenience alias
-        self(message, Level.Critical)
+            @staticmethod
+            def error(message:str) -> None:
+                # Convenience alias
+                parent._log(message, Level.Error)
+
+            @staticmethod
+            def critical(message:str) -> None:
+                # Convenience alias
+                parent._log(message, Level.Critical)
+
+            def add_handler(self, handler:logging.Handler, formatter:T.Optional[logging.Formatter] = None, level:T.Optional[Level] = None) -> None:
+                # TODO Don't add the same handler more than once
+                handler.setFormatter(formatter or parent._formatter)
+                handler.setLevel((level or parent._level).value)
+                parent.logger.addHandler(handler)
+
+            def to_tty(self, formatter:T.Optional[logging.Formatter] = None, level:T.Optional[Level] = None) -> None:
+                # Convenience alias
+                self.add_handler(logging.StreamHandler(), formatter, level)
+
+            def to_file(self, filename:T.Path, formatter:T.Optional[logging.Formatter] = None, level:T.Optional[Level] = None) -> None:
+                # Convenience alias
+                self.add_handler(logging.FileHandler(filename), formatter, level)
+
+        return _wrapper()
 
 
-_LOGGER = "vault"
-_LEVEL  = Level.Debug if __debug__ else Level.Info
-_FORMAT = logging.Formatter(fmt="%(asctime)s\t%(levelname)s\t%(message)s", datefmt=time.ISO8601)
+def _set_exception_handler(loggable:T.Type[_LoggableMixin]) -> None:
+    """
+    Create an exception handler that logs uncaught exceptions (except
+    keyboard interrupts) and spews the traceback to stderr (in debugging
+    mode) before terminating
 
-log = Logger(_LOGGER, _LEVEL, _FORMAT)
+    @param   loggable  Loggable mixin class
+    """
+    def _log_uncaught_exception(exc_type:T.Type[Exception], exc_val:Exception, traceback:TracebackType) -> None:
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_val, traceback)
+
+        else:
+            loggable().log.critical(str(exc_val) or exc_type.__name__)
+            if __debug__:
+                print_tb(traceback)
+
+            sys.exit(1)
+
+    sys.excepthook = _log_uncaught_exception
+
+
+def _to_tty(loggable:T.Type[_LoggableMixin]) -> None:
+    """
+    Add a stream handler to the specified loggable mixin
+
+    @param   loggable  Loggable mixin class
+    """
+    loggable().log.to_tty()
+
+
+class base(T.SimpleNamespace):
+    """ Namespace of base classes to make importing easier """
+    LoggableMixin = _LoggableMixin
+
+
+class utils(T.SimpleNamespace):
+    """ Namespace of utilities to make importing easier """
+    make_format           = partial(logging.Formatter, datefmt=time.ISO8601)
+    set_exception_handler = _set_exception_handler
+    to_tty                = _to_tty
+
+
+class formats(T.SimpleNamespace):
+    """ Namespace of formatters to make importing easier """
+    default = utils.make_format("%(asctime)s\t%(levelname)s\t%(message)s")
+    with_username = utils.make_format(f"%(asctime)s\t%(levelname)s\t{getuser()}\t%(message)s")
+
+
+class levels(T.SimpleNamespace):
+    """ Namespace of levels to make importing easier """
+    default = Level.Debug if __debug__ else Level.Info
