4cd9c63d029a6026c5c734bb96809c07f56751ff
Aiden Neale <an12@sanger.ac.uk>
2020-07-22 14:29:43 +0100

    Failing validation testing & code review cleanup of code
    Added:
    * Testing for 4 fail clauses of _validate
    
    Removed:
    * Redundant code
    
    Changed:
    * Moved example configs to global namespace
    * Changed file writing to path.write_text instead of 'with open'

--- test/api/test_config.py
+++ test/api/test_config.py
@@ -20,77 +20,19 @@ along with this program. If not, see https://www.gnu.org/licenses/
 import os
 import unittest
 from tempfile import TemporaryDirectory
+
 from core import typing as T, time
 from core.config import exception
 from api.config import Config, _validate, _schema
 
-
-class TestLoader(unittest.TestCase):
-    _tmp:TemporaryDirectory
-    _path:T.Path
-
-    def setUp(self) -> None:
-        self._tmp = TemporaryDirectory()
-        self._path = path = T.Path(self._tmp.name)
-
-        tmp_file = path / "config"
-        tmp_file.touch()
-
-
-    def tearDown(self) -> None:
-        self._tmp.cleanup()
-        del self._path
-
-
-
-    def test_branch_contents(self) -> None:
-        config = Config(T.Path("eg/.vaultrc"))
-        self.assertEqual(config.identity.groups.attributes.owners, "owner")
-        self.assertEqual(config.identity.users.dn, "ou=users,dc=example,dc=com")
-
-        self.assertEqual(config.persistence.postgres.port, 5432)
-        self.assertEqual(config.persistence.password, "abc123")
-
-        self.assertEqual(config.email.smtp.host, "mail.example.com")
-        self.assertEqual(config.deletion.warnings, [time.delta(days=10), time.delta(days=3), time.delta(days=1)])
-
-        self.assertEqual(config.archive.handler, T.Path("/path/to/executable"))
-
-
-
-    def test_build(self) -> None:
-        _path = self._path / "config"
-
-        self.assertTrue(Config(T.Path("eg/.vaultrc")))
-
-        _not_a_config = """
-This is an example
-"""
-        _bad_YAML = """
+_not_a_config = """ This is an example """
+_bad_YAML = """
 identity:
-            ldap:
-        host: baz
-    port: quux
+    ldap:
+            host: baz
+        port: quux
 """
-
-        with open(_path, 'w') as file:
-            file.write(_not_a_config)
-
-        self.assertRaises(exception.InvalidConfiguration, Config._build, _path)
-
-        with open(_path, 'w') as file:
-            file.write(_bad_YAML)
-
-        self.assertRaises(exception.InvalidConfiguration, Config._build, _path)
-
-
-
-    def test_validation(self) -> None:
-        _path = self._path / "config"
-        
-        self.assertTrue(Config(T.Path("eg/.vaultrc"))._is_valid)
-
-        _basic_schema = """
+_basic_config = """
 identity:
     ldap:
         host: ldap.example.com
@@ -118,8 +60,7 @@ archive:
     threshold: 1000
     handler: /path/to/executable
 """
-
-        _added_info_schema = """
+_added_info_config = """
 identity:
     ldap:
         host: ldap.example.com
@@ -145,24 +86,192 @@ email:
 deletion:
     threshold: 90
     warnings:
-        - 240
-        - 72
-        - 24
+    - 240
+    - 72
+    - 24
+archive:
+    threshold: 1000
+    data: more information
+    handler: /path/to/executable
+"""
+_scalar_list_config = """
+identity:
+    ldap:
+        host: ldap.example.com
+    users:
+        dn: ou=users,dc=example,dc=com
+        attributes:
+            uid: uidNumber
+    groups:
+        dn: ou=groups,dc=example,dc=com
+        attributes:
+            gid: gidNumber
+persistence:
+    postgres:
+        host: postgres.example.com
+        port:
+            - 1
+            - 2
+    database: sandman
+    data: information
+    user: a_db_user
+    password: abc123
+email:
+    smtp:
+        host: mail.example.com
+    sender: vault@example.com
+deletion:
+    threshold: 90
+    warnings:
+    - 240
+    - 72
+    - 24
+archive:
+    threshold: 1000
+    data: more information
+    handler: /path/to/executable
+"""
+_incorrect_type_config = """
+identity:
+    ldap:
+        host: ldap.example.com
+    users:
+        dn: ou=users,dc=example,dc=com
+        attributes:
+            uid: uidNumber
+    groups:
+        dn: ou=groups,dc=example,dc=com
+        attributes:
+            gid: gidNumber
+persistence:
+    postgres:
+        host: postgres.example.com
+    database: sandman
+    data: information
+    user: a_db_user
+    password: abc123
+email:
+    smtp:
+        host: mail.example.com
+    sender: vault@example.com
+deletion:
+    threshold: 90
+    warnings:
+    - 240
+    - 72
+    - 24
+archive:
+    threshold: 1000
+    data: more information
+    handler: 1234
+"""
+_missing_key_config = """
+identity:
+    users:
+        dn: ou=users,dc=example,dc=com
+        attributes:
+            uid: uidNumber
+    groups:
+        dn: ou=groups,dc=example,dc=com
+        attributes:
+            gid: gidNumber
+persistence:
+    postgres:
+        host: postgres.example.com
+    database: sandman
+    data: information
+    user: a_db_user
+    password: abc123
+email:
+    smtp:
+        host: mail.example.com
+    sender: vault@example.com
+deletion:
+    threshold: 90
+    warnings:
+    - 240
+    - 72
+    - 24
 archive:
     threshold: 1000
     data: more information
     handler: /path/to/executable
 """
 
-        with open(_path, 'w') as file:
-            file.write(_basic_schema)
+class TestLoader(unittest.TestCase):
+    _tmp:TemporaryDirectory
+    _path:T.Path
 
+    def setUp(self) -> None:
+        self._tmp = TemporaryDirectory()
+        self._path = path = T.Path(self._tmp.name)
+
+    def tearDown(self) -> None:
+        self._tmp.cleanup()
+        del self._path
+
+    def test_branch_contents(self) -> None:
+        config = Config(T.Path("eg/.vaultrc"))
+        self.assertEqual(config.identity.ldap.host, "ldap.example.com")
+        self.assertEqual(config.identity.ldap.port, 389)
+
+        self.assertEqual(config.identity.users.dn, "ou=users,dc=example,dc=com")
+        self.assertEqual(config.identity.users.attributes.uid, "uidNumber")
+        self.assertEqual(config.identity.users.attributes.name, "cn")
+        self.assertEqual(config.identity.users.attributes.email, "mail")
+
+        self.assertEqual(config.identity.groups.dn, "ou=groups,dc=example,dc=com")
+        self.assertEqual(config.identity.groups.attributes.gid, "gidNumber")
+        self.assertEqual(config.identity.groups.attributes.owners, "owner")
+        self.assertEqual(config.identity.groups.attributes.members, "member")
+
+        self.assertEqual(config.persistence.postgres.host, "postgres.example.com")
+        self.assertEqual(config.persistence.postgres.port, 5432)
+
+        self.assertEqual(config.persistence.database, "sandman")
+        self.assertEqual(config.persistence.user, "a_db_user")
+        self.assertEqual(config.persistence.password, "abc123")
+
+        self.assertEqual(config.email.smtp.host, "mail.example.com")
+        self.assertEqual(config.email.smtp.port, 25)
+        self.assertEqual(config.email.sender, "vault@example.com")
+
+        self.assertEqual(config.deletion.threshold, time.delta(days=90))
+        self.assertEqual(config.deletion.warnings, [time.delta(days=10), time.delta(days=3), time.delta(days=1)])
+
+        self.assertEqual(config.archive.threshold, 1000)
+        self.assertEqual(config.archive.handler, T.Path("/path/to/executable"))
+
+    def test_build(self) -> None:
+        _path = self._path / "config"
+
+        self.assertTrue(Config(T.Path("eg/.vaultrc")))
+
+        _path.write_text(_not_a_config)
+        self.assertRaises(exception.InvalidConfiguration, Config._build, _path)
+
+        _path.write_text(_bad_YAML)
+        self.assertRaises(exception.InvalidConfiguration, Config._build, _path)
+
+    def test_validation(self) -> None:
+        _path = self._path / "config"
+
+        self.assertTrue(Config(T.Path("eg/.vaultrc"))._is_valid)
+
+        _path.write_text(_basic_config)
         self.assertTrue(Config(T.Path(_path))._is_valid)
 
-        with open(_path, 'w') as file:
-            file.write(_added_info_schema)
-
+        _path.write_text(_added_info_config)
         self.assertTrue(Config(T.Path(_path))._is_valid)
+
+        _path.write_text(_scalar_list_config)
+        self.assertRaises(exception.InvalidConfiguration.InvalidSemanticConfiguration, Config, T.Path(_path))
+
+        _path.write_text(_incorrect_type_config)
+        self.assertRaises(exception.InvalidConfiguration.InvalidSemanticConfiguration, Config, T.Path(_path))
+
+        _path.write_text(_missing_key_config)
+        self.assertRaises(exception.InvalidConfiguration.InvalidSemanticConfiguration, Config, T.Path(_path))
 
 if __name__ == "__main__":
     unittest.main()
