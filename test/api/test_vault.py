04f3b15b574fee4531c6e0550ccfe2d7af3c0a15
Piyush Ahuja <piyushahuja.in@gmail.com>
2020-07-30 15:44:48 +0100

    Add tests for api.Vault
    Added:
    * Tests for constructor, add, list, remove

--- test/api/test_vault.py
+++ test/api/test_vault.py
@@ -23,7 +23,8 @@ from tempfile import TemporaryDirectory
 
 from core import typing as T, idm as IdM
 from core.utils import base64
-from api.vault import _VaultFileKey
+from api.vault import _VaultFileKey, VaultFile, Vault, Branch
+
 
 
 _DUMMY = T.Path("foo/bar/quux")
@@ -60,6 +61,17 @@ class TestVaultFileKey(unittest.TestCase):
 class _DummyUser(IdM.base.User):
     def __init__(self, uid):
         self._id = uid
+   
+    @property
+    def name(self):
+        raise NotImplementedError
+
+    @property
+    def email(self):
+        raise NotImplementedError
+
+
+
 
 class _DummyGroup(IdM.base.Group):
     _owner:_DummyUser
@@ -72,7 +84,7 @@ class _DummyGroup(IdM.base.Group):
 
     @property
     def owners(self):
-        yield self._owner
+        return iter([self._owner])
 
     @property
     def members(self):
@@ -95,6 +107,85 @@ class _DummyIdM(IdM.base.IdentityManager):
 # * Vault root setting
 # * Vault and branch creation
 # * Vault owners
+
+
+
+
+class TestVault(unittest.TestCase):
+    _tmp: TemporaryDirectory
+    _path: T.Path
+    user_one = _DummyUser(1)
+    user_two = _DummyUser(2)
+    group_one = _DummyGroup(1, user_one)
+    group_two = _DummyGroup(2, user_two)
+    idm_user_one = _DummyIdM(1) 
+
+
+    def setUp(self) -> None:
+        self._tmp = TemporaryDirectory()
+        self._path = path = T.Path(self._tmp.name).resolve()
+    
+        self.parent_dir = path / "parent_dir"
+        self.child_dir_one = self.parent_dir / "child_dir_one"
+        self.child_dir_two = self.parent_dir / "child_dir_two"
+        self.tmp_file_one = self.child_dir_one / "a"
+        self.tmp_file_two = self.child_dir_one / "b"
+        self.tmp_file_three = self.child_dir_two / "c"
+
+
+        self.child_dir_one.mkdir(parents=True, exist_ok=True)
+        self.child_dir_two.mkdir(parents=True, exist_ok=True)
+        self.tmp_file_one.touch()
+        self.tmp_file_two.touch()
+        self.tmp_file_three.touch()
+        # tmp_file.chmod(0o777)
+        # child_dir.chmod(0o777)
+        # parent_dir.chmod(0o777)
+        # self.vault.root = self._path / T.Path("foo/bar")
+
+        os.chown(self.parent_dir, uid=2, gid=2) 
+        os.chown(self.child_dir_one, 1, 1) 
+        os.chown(self.child_dir_two, 1, 2) 
+        os.chown(self.tmp_file_one, 1, 1) 
+        os.chown(self.tmp_file_two, 2, 1) 
+        os.chown(self.tmp_file_three, 2, 2) 
+        self.vault = Vault(relative_to = self._path / T.Path("parent_dir/child_dir_one/a"), idm = self.idm_user_one)
+
+
+
+    def tearDown(self) -> None:
+        self._tmp.cleanup()
+        del self._path
+
+    def test_constructor(self):
+        self.assertEqual(self.vault.location, self._path / T.Path("parent_dir/child_dir_one/.vault"))
+        self.assertTrue(os.path.isdir(self._path / T.Path("parent_dir/child_dir_one/.vault/keep")))
+        self.assertTrue(os.path.isdir(self._path / T.Path("parent_dir/child_dir_one/.vault/archive")))
+        self.assertTrue(os.path.isdir(self._path / T.Path("parent_dir/child_dir_one/.vault/.staged")))
+        self.assertEqual(self.vault.group, 1)
+       
+        # self.assertEqual(self.vault.owners, iter([1]))
+     
+
+    def test_add(self):
+        self.vault.add(Branch.Keep, self.tmp_file_two)
+        vault_file_key = self._path / T.Path("parent_dir/child_dir_one/.vault/keep") / VFK(0x1234, T.Path("b")).path
+        self.assertTrue(os.path.isfile(vault_file_key))    
+
+    def test_remove(self):
+        self.vault.remove(Branch.Keep, self.tmp_file_two)
+
+        
+
+# _DummyBranch, _DummyPath
+# class TestVaultFile(unittest.TestCase):
+#     def test_constructor(self):
+#         self.assertEqual(VaultFile(vault= _DummyVault, branch = _DummyBranch, path = _DummyPath).path, T.Path(_DummyPath))
+#         self.assertEqual(VaultFile(vault= _DummyVault, branch = _DummyBranch, path = _DummyPath).source, T.Path(_DummyPath))
+#         self.assertEqual(VaultFile(vault= _DummyVault, branch = _DummyBranch, path = _DummyPath), _DummyVaultFile)
+#         self.assertEqual(VaultFile(vault= _DummyVault, branch = _DummyBranch, path = _DummyPath).can_remove, True)
+#         self.assertEqual(VaultFile(vault= _DummyVault, branch = _DummyBranch, path = _DummyPath).can_add, False))
+
 
 
 if __name__ == "__main__":
