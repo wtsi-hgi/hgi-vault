8370798a61e6408315a4513ee0dace328cec3822
Piyush Ahuja <piyushahuja.in@gmail.com>
2021-02-03 06:49:39 +0000

    Added test for method work_dir_relative -> vault_relative_path
    Added:
    * Test for method that takes a canonicalised Vault path, relative to some directory under the Vault root, and converts it back to a "full" Vault path

--- test/bin/test_path.py
+++ test/bin/test_path.py
@@ -35,3 +35,28 @@ class TestVaultRelativeToWorkDirRelative(unittest.TestCase):
         self.assertEqual(expected, work_dir_rel)
 
 
+class TestWorkDirRelativeToVaultRelative(unittest.TestCase):
+
+    def test_child_to_work_dir(self):
+        #file1, some/path -> some/path/file1
+        work_dir = T.Path("some/path")
+        work_dir_rel = T.Path("file1")
+        vault_path = T.Path(".")
+        vault_relative_path = convert_work_dir_rel_to_vault_rel(work_dir_rel, work_dir, vault_path)
+        expected = T.Path("some/path/file1")
+        self.assertEqual(expected, vault_relative_path)
+
+
+    def test_sibling_to_work_dir(self):
+        #../file3, this/is/my/path -> this/is/my/file3
+        work_dir = T.Path("this/is/my/path")
+        work_dir_rel = T.Path("../file3")
+        vault_path = T.Path(".")
+        vault_relative_path = convert_work_dir_rel_to_vault_rel(work_dir_rel, work_dir, vault_path)
+        expected = T.Path("this/is/my/file3")
+        self.assertEqual(expected, vault_relative_path)
+
+
+
+
+
